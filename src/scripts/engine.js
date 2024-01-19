d30a871cb4ce10850bd2adcaa544ec89b99049f1
DudaAhkera <rochapereira16@gmail.com>
2024-01-19 17:34:45 -0300

    first commit

---
+++ src/scripts/engine.js
@@ -0,0 +1,70 @@
+const emojis = [
+    "🥰",
+    "🥰",
+    "😘",
+    "😘",
+    "😁",
+    "😁",
+    "🤔",
+    "🤔",
+    "😤",
+    "😤",
+    "🥸",
+    "🥸",
+    "🤭",
+    "🤭",
+    "🥳",
+    "🥳"
+];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(()=>(
+    Math.random() 
+    > 0.5 ? 2 : -1));
+
+for(let i = 0; i < emojis.length; i++){
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
+}
+//abrir somente duas posicões
+function handleClick () {
+    if(openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+        }
+    if (openCards.length === 2 ){
+        setTimeout(checkMatch, 500)
+    }
+}
+//comparar os vetores das cartas
+function checkMatch(){
+    if(openCards[0].innerHTML === openCards[1].innerHTML){
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    }else {
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
+
+    //resetar cartas salvas na memória
+    openCards = []; 
+
+    //reiniciar jogo depois de virara todas as cartas
+    if(document.querySelectorAll(".boxMatch").length === emojis.length){
+        alert("Você venceu!")
+    }
+}
+//disparar som (certo ou errado) ao clicar nas cartas
+function virarCarta(carta) {
+    if (openCards[0].innerHTML === openCards[1].innerHTML) {
+      document.getElementById('somCerto').play();
+      // Lógica para virar a carta correta
+    } else {
+      document.getElementById('somErrado').play();
+      // Lógica para virar a carta errada
+    }
+  }
+
